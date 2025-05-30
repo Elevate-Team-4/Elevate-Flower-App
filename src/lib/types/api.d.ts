727efb4d9183b31b98ef43f9200aea732a56d013
siefBaher <seifbaher15@gmail.com>
2025-05-30 16:25:30 +0300

    feat(auth): setup NextAuth for authentication

---
+++ src/lib/types/api.d.ts
@@ -0,0 +1,30 @@
+declare type SuccessfulResponse<T> = {
+    message: 'success';
+} & T;
+
+declare type ErrorResponse = {
+    error: string;
+};
+
+declare type APIResponse<T> = SuccessfulResponse<T> | ErrorResponse;
+
+//^ the user response after a successful login
+declare type ApplicationUser = {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    phone: string;
+    photo?: string;
+    role: string;
+    wishlist: any[]; //todo <= This needs to be edited
+    addresses: any[]; //todo <= This needs to be edited
+    createdAt: string;
+};
+
+declare type LoginResponse = {
+    message?: string;
+    user:ApplicationUser ;
+    token: string;
+};
