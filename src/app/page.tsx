a404fb43f8994defb9400a12f4ce19cf4accf0e4
Ahmed-Qotb <bl7ax99fg@gmail.com>
2025-06-03 20:20:27 +0300

    refactor(best-selling-most-popular):fixed best selling typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import BestSelleing from "./_components/best-selling/best-selling";
+import BestSelling from "./_components/best-selling/best-selling";
 import MostPopular from "./_components/most-popular/most-popular";
 
 export default function Home() {
   return (
     <div>
-      <BestSelleing />
+      <BestSelling />
       <MostPopular />
     </div>
   );
