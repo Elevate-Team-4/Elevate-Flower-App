8cac3a5d7b306486230ad021db9d3aadcb268de7
MohamedAli <mmkandeelz74@gmail.com>
2025-07-19 09:46:23 +0300

    refactor(address-model):secured fetch

--- src/app/api/get-addresses/route.ts
+++ src/app/api/get-addresses/route.ts
@@ -4,16 +4,16 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const token = await getToken({ req });
-  // if (!token) {
-  //   return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  // }
+  if (!token) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
   const response = await fetch(`${process.env.API}/addresses`, {
     next: {
       tags: ["user-addresses"],
     },
     headers: {
       ...JSON_HEADER,
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiNjg3NWMzMTFhOGJjYTMwN2Y5ZDA3ZDkwIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NTI4MDA1Nzl9.rNDRfJ52evIWlMhsgvTYIzZP_rrQnl-BEbz1XPnljSE`,
+      Authorization: `Bearer ${token}`,
     },
   });
   const payload = await response.json();
