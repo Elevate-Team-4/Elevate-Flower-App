8bcfdebbd3a9ef6c3c70fc967e3910fec7fcce73
MahmoudSayedAbdi <mahmoudsayed.2003.8@gmail.com>
2025-08-07 17:39:02 +0300

    feat(overview-2nd-row) : add Suspense

--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,9 +1,17 @@
+import { Suspense } from "react";
+import SecondRowSkeleton from "@/components/skeletons/overview-2nd-row/2nd-row-skeleton";
 import Overview2ndRow from "./_components/2nd-row/overview-2nd-row";
 
 export default async function dashboard() {
   return (
     <div className="w-full h-screen p-4 bg-gray-100 dark:bg-zinc-700">
-      <Overview2ndRow />
+      {/* 1st Row */}
+
+      {/* 2nd Row */}
+      <Suspense fallback={<SecondRowSkeleton />}>
+        <Overview2ndRow />
+      </Suspense>
+      {/* 3rd Row */}
     </div>
   );
 }
